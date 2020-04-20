c3c3543111cf22cf1463af3ccc0bf7e734cc9e68
Da Risk <da_risk@geekorum.com>
2020-04-19 20:27:36 -0400

    complete DeviceProvider api

--- plugin/src/main/kotlin/com/geekorum/gradle/avdl/DeviceProvider.kt
+++ plugin/src/main/kotlin/com/geekorum/gradle/avdl/DeviceProvider.kt
@@ -6,16 +6,51 @@ package com.geekorum.gradle.avdl
 
 /**
  * To create a device provider for Gradle Avdl, you must implement this class and provide an
- * instance of [DeviceSetupFunction] so that users can call it in their [VirtualDeviceDefinition]
+ * instance of DeviceSetupFunction so that users can call it in their [VirtualDeviceDefinition]
+ *
+ * A DeviceSetupFunction takes a [VirtualDeviceDefinition] as receiver and returns a [DeviceSetup] instance.
+ * It can has many parameters
  */
-class DeviceProviderPlugin
+abstract class DeviceProviderPlugin {
+        abstract fun createController(configuration: ByteArray) : VirtualDeviceController
+}
 
-typealias DeviceSetupFunction = VirtualDeviceDefinition.() -> DeviceSetup
+interface VirtualDeviceController {
+        fun startDevice()
+
+        fun stopDevice()
+}
 
 /**
  * Allows a DeviceProvider to register a VirtualDevice configuration
  */
 data class DeviceSetup(
-        val deviceProviderPlugin: DeviceProviderPlugin,
-        val configuration: Any
-)
+        /**
+         * Fully qualified name of the class implementing the [DeviceProviderPlugin]
+         */
+        val deviceProviderPlugin: String,
+
+        /**
+         * Configuration blob
+         */
+        val configuration: ByteArray
+) {
+
+        override fun equals(other: Any?): Boolean {
+                if (this === other) return true
+                if (javaClass != other?.javaClass) return false
+
+                other as DeviceSetup
+
+                if (deviceProviderPlugin != other.deviceProviderPlugin) return false
+                if (!configuration.contentEquals(other.configuration)) return false
+
+                return true
+        }
+
+        override fun hashCode(): Int {
+                var result = deviceProviderPlugin.hashCode()
+                result = 31 * result + configuration.contentHashCode()
+                return result
+        }
+}
