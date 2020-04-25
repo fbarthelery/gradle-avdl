f3b7a76873afed814131388736abe4946667a791
Da Risk <da_risk@geekorum.com>
2020-04-25 18:50:45 -0400

    plugin: add a StopDeviceTask

---
+++ plugin/src/main/kotlin/com/geekorum/gradle/avdl/tasks/StopDeviceTask.kt
@@ -0,0 +1,47 @@
+package com.geekorum.gradle.avdl.tasks
+
+import com.geekorum.gradle.avdl.AvdlExtension
+import org.gradle.api.DefaultTask
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Nested
+import org.gradle.api.tasks.TaskAction
+import org.gradle.kotlin.dsl.get
+import org.gradle.kotlin.dsl.listProperty
+import org.gradle.kotlin.dsl.the
+import org.gradle.workers.WorkerExecutor
+import javax.inject.Inject
+
+class StopDeviceTask@Inject constructor(
+        objectFactory: ObjectFactory,
+        private val workerExecutor: WorkerExecutor
+) : DefaultTask() {
+
+    @get:Input
+    val devices = objectFactory.listProperty<String>()
+
+    @get:Nested
+    internal val deviceDefinitions = project.the<AvdlExtension>().devices
+
+    @Suppress("UnstableApiUsage")
+    @TaskAction
+    fun stopDevices() {
+        val classpath = project.buildscript.configurations["classpath"]
+        devices.get().forEach {
+            val deviceDefinition = deviceDefinitions[it]
+            workerExecutor.noIsolation().submit(StopDeviceWork::class.java) {
+                setDeviceDefinitionParams(deviceDefinition)
+                buildScriptClasspath.set(classpath.asPath)
+            }
+        }
+    }
+}
+
+@Suppress("UnstableApiUsage")
+private abstract class StopDeviceWork @Inject constructor(
+        objectFactory: ObjectFactory
+) : DeviceWorker<ProviderWorkerParams>(objectFactory) {
+    override fun execute() {
+        controller.stopDevice()
+    }
+}
